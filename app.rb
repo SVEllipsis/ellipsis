5a1b1fbc55f8df5a8722084cd2823fc59598404f
Ben Scott <ben@reload.me.uk>
2014-04-20 16:48:57 +0100

    Basic API + Dashboard
    * Add a basic API that spits out JSON at /data.json Parameters offered:
      * start_time
      * end_time
      * fields (not fully working)
      * resolution (not implemented yet)
    * Add a basic dashboard that shows the latest data

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ require './lib/sinatra-reloader'
 
 
 class App < Sinatra::Application
+  set :afloat, !!ENV['AFLOAT'] || false
+
   configure do
+    use Rack::Deflater
+
     Dir[File.join(File.dirname(__FILE__), "initializers/*.rb")].each do |file|
       require file
     end
@@ -19,23 +23,74 @@ class App < Sinatra::Application
   end
 
   get '/?' do
-    redirect '/weather'
+   # These parameters come from the query string
+    params[:start_time] ||= nil
+    params[:end_time] ||= nil
+    params[:resolution] ||= nil
+
+    @page = {:title => 'Dashboard'}
+
+    # Commented out duration as we're currently working on with demo data
+    @data = get_data({
+      # start_time => start_time || Time.now - 60*60*3 # Three hours ago
+      # end_time = end_time || Time.now
+    })
+
+    @data_latest = @data[1]
+
+    @partials = [
+      :'dashboard/geodata',
+      :'dashboard/weather',
+    ]
+
+    erb :dashboard
   end
 
   # Hot pants! It's named capture groups in regexp
-  get %r{^/weather(?:\.(?<format>json))?$} do
-    data = {}
+  get %r{^/data(?:\.(?<format>json))?$} do
+    # These parameters come from the query string
+    params[:start_time] ||= nil
+    params[:end_time] ||= nil
+    params[:fields] ||= nil
+    params[:resolution] ||= nil
 
-    return json data if params[:format] == 'json'
-    @page = { :title => params[:format].to_s + 'Weather'}
-    erb :weather
+    #TODO Implement page / limit parameters
+
+    # TODO fields are lazy loaded so they end up being populated anyway.
+    # Make sure that only the requested fields are returned in the dataset
+
+    data = get_data({
+      :start_time => (params[:start_time] ? Time.parse(params[:start_time]) : nil),
+      :end_time =>  (params[:end_time] ? Time.parse(params[:end_time]) : nil),
+      :fields => (params[:fields] ? params[:fields].split(',') : nil),
+    })
+
+    return json({:count => data.length, :data => data}) if params[:format] == 'json'
+    #TODO return jsonp?
   end
 
-  get %r{^/orientation(?:\.(?<format>json))?$} do
-    data = {}
+  def get_data(opts={})
+    # TODO resolution option for limiting amount of datapoints
+    # datapoint every (1min / 5min / 30min) etc
+    # http://stackoverflow.com/questions/10403039/mysql-select-query-5-minute-increment
 
-    return json data if params[:format] == 'json'
-    @page = { :title => 'Orientation' }
-    erb :orientation
+    opts = {
+      :start_time => nil,
+      :end_time => nil,
+      :fields => nil,
+      :resolution => nil,
+    }.merge(opts)
+
+
+    parameters ={
+      :order => [ :created_at.desc ]
+    }
+
+    parameters[:fields] = opts[:fields] if opts[:fields]
+    parameters[:created_at.gt] = opts[:start_time] if opts[:start_time]
+    parameters[:created_at.lt] = opts[:end_time] if opts[:end_time]
+
+    Nmea.all(parameters)
   end
+
 end
