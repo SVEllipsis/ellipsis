1904385863a388e76b5d80bcce44f72e83e6e25b
Kevin Prince <nextgengames@gmail.com>
2014-04-20 09:08:41 +0100

    NMEA library to log the nmea bus data

--- models/nmea.rb
+++ models/nmea.rb
@@ -1,4 +1,16 @@
 class Nmea
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :lat, Float
+  property :long, Float
+  property :speed, Float
+  property :bearing, Float
+  property :winddir, Float
+  property :windspeed, Float
+  property :waterspeed, Float
+  property :watertemp, Float
+  property :created_at, DateTime
 
   def self.set(key, value)
     REDIS.hset 'nmea', key, value
@@ -11,10 +23,46 @@ class Nmea
   def self.sync
     keys = REDIS.hgetall 'nmea'
 
-    keys.each do |k,v|
-      Event.set(k, v)
-    end
+    self.create(
+      :lat => keys['LATA'],
+      :long => keys['LONG'],
+      :speed => keys['KNOT'],
+      :bearing => keys['BEAR'],
+      :winddir => keys['WIND'],
+      :windspeed => keys['WINS'],
+      :waterspeed => keys['WATS'],
+      :watertemp => keys['WATT'],
+      :created_at => Time.now()
+    )
 
+  end
+
+  def self.parse(msg)
+    if valid_message?(msg)
+      kv = msg.split("=")
+      self.set(kv[0], kv[1])
+    end
+  end
+
+  private
+
+  def self.valid_message?(msg)
+    /[A-Z][A-Z][A-Z][A-Z]=/.match(msg)
+  end
+
+  def valid_values
+    {
+      'LATA' => 'latitude',
+      'LONG' => 'longitude',
+      'DATE' => 'date',
+      'TIME' => 'time',
+      'KNOT' => 'speed in knots',
+      'BEAR' => 'direction of travel',
+      'WIND' => 'wind direction',
+      'WINS' => 'wind speed',
+      'WATT' => 'water temperature',
+      'WATS' => 'water speed'
+    }
   end
 
 end
