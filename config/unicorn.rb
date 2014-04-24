4756893a7b6e243cb1e760fab43f10a9668ad6b2
kevinprince <nextgengames@gmail.com>
2014-04-24 21:29:37 +0100

    expanding unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,17 @@
-worker_processes 3
+@dir = "/home/pi/ellipsis/"
+
+worker_processes 2
+working_directory @dir
 timeout 30
 preload_app true
+
+# Specify path to socket unicorn listens to,
+# we will use this in our nginx.conf later
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
