1cfa82a59efffa6b36479a1d9ef188c27c550d46
kevinprince <nextgengames@gmail.com>
2014-04-24 21:37:31 +0100

    earlier was better

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,14 +4,3 @@ worker_processes 2
 working_directory @dir
 timeout 30
 preload_app true
-
-# Specify path to socket unicorn listens to,
-# we will use this in our nginx.conf later
-listen "/tmp/sockets/unicorn.sock", :backlog => 64
-
-# Set process id path
-pid "/tmp/pids/unicorn.pid"
-
-# Set log file paths
-stderr_path "/var/log/unicorn.stderr.log"
-stdout_path "/var/log/unicorn.stdout.log"
