a8f9a46a2dfb33d3bcc5d3c290a77fa107cca10c
Kevin Prince <nextgengames@gmail.com>
2014-04-25 12:53:55 +0100

    Adding weather json file parser and model

---
+++ models/weather.rb
@@ -0,0 +1,16 @@
+class Weather
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :temp_out, Float
+  property :temp_in, Float
+  property :temp_appr, Float
+  property :wind_avg, Float
+  property :wind_gust, Float
+  property :hum_out, Float
+  property :hum_in, Float
+  property :rain, Float
+  property :rel_pressure, Float
+  property :created_at, DateTime
+
+end
