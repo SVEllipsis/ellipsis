6571c7d9b078b83fa405da5640e60ce8d07fa4cf
Kevin Prince <nextgengames@gmail.com>
2014-04-25 14:06:44 +0100

    Send message method saves to db and posts to redis for rock block to transmit

--- models/messages.rb
+++ models/messages.rb
@@ -5,4 +5,15 @@ class Messages
   property :type, String, :length => 2
   property :text, Text
   property :created_at, DateTime
+
+  def self.send_message(msg = '')
+    self.create(
+      :type => 'mo',
+      :text => msg,
+      :created_at => Time.now()
+    )
+
+    REDIS.publish("mo", msg)
+  end
+
 end
