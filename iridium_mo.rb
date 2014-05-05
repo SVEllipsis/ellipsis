6f7eea28d68732c0ba11e0a50e3cd93a63de008b
Kevin Prince <nextgengames@gmail.com>
2014-05-05 17:51:58 +0100

    got scopes wrong

--- iridium_mo.rb
+++ iridium_mo.rb
@@ -8,26 +8,26 @@ end
 
 values = Array.new
 
-values << nmea['lat']
-values << nmea['long']
-values << nmea['bearing']
-values << nmea['speed']
-values << nmea['waterspeed']
-values << weather['temp_out']
+values << @nmea['lat']
+values << @nmea['long']
+values << @nmea['bearing']
+values << @nmea['speed']
+values << @nmea['waterspeed']
+values << @weather['temp_out']
 values << windspeed
 values << winddir
-values << nmea['rel_pressure']
+values << @nmea['rel_pressure']
 
 message = values.join(',')
 
 Messages.send_message(message)
 
 def windspeed
-  return nmea['windspeed'] unless nil
-  weather['wind_dir']
+  return @nmea['windspeed'] unless nil
+  @weather['wind_dir']
 end
 
 def winddir
-  return nmea['winddir'] unless nil
-  weather['wind_dir']
+  return @nmea['winddir'] unless nil
+  @weather['wind_dir']
 end
