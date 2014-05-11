63599d034511e376bcb9e6056003a4a7b790b5a5
kevinprince <nextgengames@gmail.com>
2014-05-11 14:49:53 +0100

    tweaking to use heroku env's

--- initializers/datamapper.rb
+++ initializers/datamapper.rb
@@ -1,13 +1,6 @@
 require 'data_mapper'
 
-DataMapper.setup(:default, {
-  adapter: 'mysql',
-  database: 'ellipsis',
-  username: 'root',
-  password: '',
-  host: 'localhost',
-  port: 3306
-})
+DataMapper.setup(:default, ENV['CLEARDB_DATABASE_URL'] || 'mysql://root@localhost/ellipsis')
 
 Dir[File.join(File.dirname(__FILE__), "../models/*.rb")].each do |file|
   require file
--- initializers/redis.rb
+++ initializers/redis.rb
@@ -1,3 +1,5 @@
 require 'redis'
 
-REDIS = Redis.new(:host => '127.0.0.1', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://127.0.0.1:6379/')
+
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
