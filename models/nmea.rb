9c7b9dd2dedbb37219297b375659c066d8ccea24
Tom Gladhill <whoojemaflip@gmail.com>
2014-05-12 13:59:41 +0100

    Output formatting for geo-floats

--- models/nmea.rb
+++ models/nmea.rb
@@ -12,6 +12,25 @@ class Nmea
   property :watertemp, Float
   property :created_at, DateTime
 
+  def lat_as_str
+    ord = lat >= 0 ? "N" : "S"
+    geo_format(lat, ord)
+  end
+
+  def long_as_str
+    ord = long >= 0 ? "E" : "W"
+    geo_format(long, ord)
+  end
+
+  def geo_format(geo_float, ord)
+    units = degreesminutesseconds(geo_float)
+    "#{units[:degrees]}\xc2\xb0 #{units[:minutes]}\xe2\x80\xb2 #{units[:seconds]}\xe2\x80\xb3 #{ord}"
+  end
+
+  def degreesminutesseconds(fl)
+    fl.to_s.match /(?<degrees>\d{0,3})\.(?<minutes>\d\d)(?<seconds>\d{0,3})/
+  end
+
   def self.set(key, value)
     REDIS.hset 'nmea', key, value
   end
